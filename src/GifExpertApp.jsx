6708d25964ee3954014f757f14db6deecbafd3dd
carloscamacho <carlosalvarez9805@gmail.com>
2022-09-26 17:33:29 -0500

    Add base style.css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,20 @@
 
+/* Este functional component lo podemos ver como si fuera
+nuestro lienzo, dentro de este vamos a meter los demas
+componentes para hacer la app*/
+
 export const GifExpertApp = () => {
     return (
         <>
+
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+            
+            {/* Input */}
+            
+            {/* Listado de Gifs */}
+                {/* Gif item */}
+
         </>
     );
 }
