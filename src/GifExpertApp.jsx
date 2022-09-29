397a8365f989ed1fad230f4b2b11bc78db8bb5ac
carloscamacho <carlosalvarez9805@gmail.com>
2022-09-28 19:14:41 -0500

    Add Emit an event to the parent

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -9,9 +9,11 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState([ 'One Punch', 'Dragon ball']);
 
-    const onAddCategory = () => {
+    const onAddCategory = ( newCategory ) => {
+        //console.log( newCategory );
+
         // dos maneras de agregar un nuevo elemento a un array
-        setCategories([ 'Minecraft', ...categories ]);       // Metodo 1
+        setCategories([ newCategory, ...categories ]);       // Metodo 1
         // setCategories( cat => [ ...cat, 'Minecraft' ]);   // Metodo 2
     }
 
@@ -22,9 +24,12 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
             
             {/* Input */}
-            <AddCategory setCategories={ setCategories } />
+            <AddCategory
+                // setCategories={ setCategories }
+                onNewCategory={ ( value ) => onAddCategory( value ) }
+            />
 
-            {/* Listado de Gifs */}
+            {/* Listado de Gifs // Nunca se deben de poner los index como clave ya que react se puede confundir si llegases a borrar un elemento de la lista*/}
             <ol>
                 {  categories.map( category => <li key={ category }>{category}</li> ) }
             </ol>
